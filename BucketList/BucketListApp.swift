0804fea760632ac8e592feec725298337f7cc4de
fernandocallejasr <ferd.callejas@gmail.com>
2024-08-31 13:09:23 -0600

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Fernando Callejas on 31/08/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
